73d5680c9e7a28e78bfb83e101e7d0448ea1bbda
Laven Morrow <lmorrow13@live.com>
2018-02-21 16:26:17 -0700

    finished build

--- main.rb
+++ main.rb
@@ -1,5 +1,29 @@
 require 'sinatra'
+require 'pony'
 
 get '/' do
 	erb :home
+end
+
+get '/menu' do
+	erb :menu
+end
+
+get '/contact' do
+	erb :contact
+end
+
+get '/about' do
+	erb :about
+end
+
+get '/aot' do
+	erb :aot
+end
+
+post '/contactUs' do
+	@email = params[:email]
+	@subject = params[:subject]
+	@content = params[:content]
+	Pony.mail(:to => 'you@example.com', :from => @email, :subject => @subject, :body => @content)
 end
